9288b85ebed5095a21da6e9125f617e3a1ef338b
Kaputchino <61330453+Kaputchino@users.noreply.github.com>
2024-12-01 20:54:08 +0100

    Update day1adventcode24.ml
    j'avais oublié la seconde partie

--- Jour 1/Ocaml/Andreas/day1adventcode24.ml
+++ Jour 1/Ocaml/Andreas/day1adventcode24.ml
@@ -1,6 +1,7 @@
-let liste1 = [1; 2; 3; 4] 
-let liste2 = [3; 7; 5; 2] 
+let liste1 = [1; 2; 3; 4; 9] 
+let liste2 = [3; 7; 5; 2; 2] 
 let sum = ref 0
+let sum2 = ref 0
 let sorted_list1 = List.sort compare liste1 ;;
 let sorted_list2 = List.sort compare liste2;; 
 List.iter2 (fun x y -> 
@@ -11,3 +12,8 @@ List.iter2 (fun x y ->
   ) sorted_list1 sorted_list2;;
 
 Printf.printf "Somme : %d\n" !sum;
+
+sum := 0;;
+List.iter(fun n -> 
+    sum := !sum + n * List.fold_left (fun acc x -> if x = n then acc + 1 else acc) 0 liste2) liste1;;
+Printf.printf "Somme : %d\n" !sum
